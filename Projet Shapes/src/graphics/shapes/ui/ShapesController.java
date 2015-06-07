5553a8404556fb4cb42583bfefe4eef96f6641f4
DodgerAkame <rmps@hotmail.fr>
2015-06-07 16:58:19 +0200

    Update de Controller et ShapesController
    J'ai toujours pas trouvé comment afficher des formes, je regarderai dans
    la soirée, si jamais vous trouvez, hésitez pas.

--- Projet Shapes/src/graphics/shapes/ui/ShapesController.java
+++ Projet Shapes/src/graphics/shapes/ui/ShapesController.java
@@ -1,12 +1,70 @@
 package graphics.shapes.ui;
 
-import graphics.ui.*;
+import graphics.shapes.SCollection;
+import graphics.shapes.Shape;
+import graphics.shapes.attributes.SelectionAttributes;
+import graphics.ui.Controller;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.util.Iterator;
 
 public class ShapesController extends Controller {
 
+	private int mouseX = 0;
+	private int mouseY = 0;
+	
 	public ShapesController(Object newModel) {
 		super(newModel);
-		// TODO Auto-generated constructor stub
+	}
+
+	public void mousePressed(MouseEvent e) {
+		this.mouseX=e.getX();
+		this.mouseY=e.getY();
+	}
+
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		SCollection sc = (SCollection) this.getModel();
+		for (Iterator it= sc.list.iterator();it.hasNext();) {
+			Shape s = (Shape) it.next();
+			SelectionAttributes sa = (SelectionAttributes)
+					s.getAttributes("SelectionAttributes");
+			if (s.getBounds().contains(e.getX(), e.getY())) {
+				if (e.isShiftDown())
+					sa.toggleSelection();
+				else
+					sa.select();
+		} else {
+			if (!e.isShiftDown())
+				sa.unselect();
+			}
+		}
+		super.getView().repaint();
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		super.getView().requestFocus();
+	}
+
+	public void mouseExited(MouseEvent e) {
+	}
+
+	public void mouseMoved(MouseEvent evt) {
+	}
+
+	public void mouseDragged(MouseEvent evt) {
+	}
+
+	public void keyTyped(KeyEvent evt) {
+	}
+
+	public void keyPressed(KeyEvent evt) {
+	}
+
+	public void keyReleased(KeyEvent evt) {
 	}
 
 }
--- Projet Shapes/src/graphics/ui/Controller.java
+++ Projet Shapes/src/graphics/ui/Controller.java
@@ -7,9 +7,7 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 public class Controller implements MouseListener, MouseMotionListener,
-		KeyListener
-// Pour lire les interactions avec la souris
-{
+		KeyListener {
 	private Object model;
 	private View view;
 
@@ -34,32 +32,52 @@ public class Controller implements MouseListener, MouseMotionListener,
 	}
 
 	public void mousePressed(MouseEvent e) {
+		System.out.println("press");
+		System.out.println(e);
 	}
 
 	public void mouseReleased(MouseEvent e) {
+		System.out.println("release");
+		System.out.println(e);
 	}
 
 	public void mouseClicked(MouseEvent e) {
+		System.out.println("click");
+		System.out.println(e);
 	}
 
 	public void mouseEntered(MouseEvent e) {
+		System.out.println("enter");
+		System.out.println(e);
 	}
 
 	public void mouseExited(MouseEvent e) {
+		System.out.println("exit");
+		System.out.println(e);
 	}
 
 	public void mouseMoved(MouseEvent evt) {
+		System.out.println("move");
+		System.out.println(evt);
 	}
 
 	public void mouseDragged(MouseEvent evt) {
+		System.out.println("drag");
+		System.out.println(evt);
 	}
 
 	public void keyTyped(KeyEvent evt) {
+		System.out.println("type");
+		System.out.println(evt);
 	}
 
 	public void keyPressed(KeyEvent evt) {
+		System.out.println("press key");
+		System.out.println(evt);
 	}
 
 	public void keyReleased(KeyEvent evt) {
+		System.out.println("release key");
+		System.out.println(evt);
 	}
 }
